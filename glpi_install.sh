661dfd6198a0457f117546f292f978746062a2ab
RayToRo13 <61200265+RayToRo13@users.noreply.github.com>
2020-04-08 15:34:26 +0200

    Installation Silencieuse Glpi

--- glpi_install.sh
+++ glpi_install.sh
@@ -4,4 +4,4 @@ cd /var/www/html/glpi
 #Commande pour installer glpi automatiquement/
 php bin/console db:install -f -n -L fr_FR -H localhost -d glpidb -u glpi -p password
 #hostname = localhost, database = glpidb, user = glpi, password = password
-#Modifer aux besoins
+#Modifer aux besoins 
